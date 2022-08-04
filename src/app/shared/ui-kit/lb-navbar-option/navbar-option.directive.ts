db20acc34d4de2265d795fd8f91437a94b06b4bd
Moein Hosseini <rxmoein@tuta.io>
2022-08-04 16:51:19 +0200

    Update prefix

--- src/app/shared/ui-kit/lb-navbar-option/navbar-option.directive.ts
+++ src/app/shared/ui-kit/lb-navbar-option/navbar-option.directive.ts
@@ -1,8 +1,8 @@
 import { Directive, HostBinding } from '@angular/core';
 
 @Directive({
-  selector: '[lb-navbar-option]'
+  selector: '[app-navbar-option]'
 })
 export class NavbarOptionDirective {
-  @HostBinding('class') class = 'lb-navbar-option';
+  @HostBinding('class') class = 'app-navbar-option';
 }
